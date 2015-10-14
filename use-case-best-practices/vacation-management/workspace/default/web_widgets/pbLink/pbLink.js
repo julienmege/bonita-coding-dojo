04a2237df4132aa5b70e6be19005d93192489549
julien mege <julien.mege@bonitasoft.com>
2015-10-14 11:34:18 +0200

    add .bos

--- use-case-best-practices/vacation-management/workspace/default/web_widgets/pbLink/pbLink.js
+++
@@ -1,6 +0,0 @@
-angular.module('bonitasoft.ui.widgets')
-  .directive('pbLink', function() {
-    return {
-      template: '<div class="text-{{ properties.alignment }}">\n    <a ng-class="properties.buttonStyle !== \'none\' ? \'btn btn-\' + properties.buttonStyle : \'\'" ng-href="{{properties.targetUrl}}">{{ properties.text | uiTranslate }}</a>\n</div>\n'
-    };
-  });
--- use-case-best-practices/vacation-management/workspace/default/web_widgets/pbText/pbText.js
+++
@@ -1,6 +0,0 @@
-angular.module('bonitasoft.ui.widgets')
-  .directive('pbText', function() {
-    return {
-      template: '<p class="text-{{ properties.alignment }}" ng-bind-html="properties.text | uiTranslate"></p>\n'
-    };
-  });
