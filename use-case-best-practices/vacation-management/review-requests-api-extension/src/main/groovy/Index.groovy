2ebde4ab6ce1f0f553d81de4018bc213c6029805
Vincent <vincent.elcrin@gmail.com>
2015-10-29 15:26:02 +0100

    Fix rest api build

--- use-case-best-practices/vacation-management/review-requests-api-extension/src/main/groovy/Index.groovy
+++ use-case-best-practices/vacation-management/review-requests-api-extension/src/main/groovy/Index.groovy
@@ -1,16 +1,7 @@
-import bizdata.VacationRequestDAO
 import groovy.json.JsonBuilder
 import org.bonitasoft.console.common.server.page.*
-import org.bonitasoft.engine.api.TenantAPIAccessor
-import org.bonitasoft.engine.bdm.BusinessObjectDAOFactory
-import org.bonitasoft.engine.business.data.SimpleBusinessDataReference
-import org.bonitasoft.engine.search.Order
-import org.bonitasoft.engine.search.SearchOptionsBuilder
 
 import javax.servlet.http.HttpServletRequest
-
-import static org.bonitasoft.engine.bpm.flownode.ActivityStates.READY_STATE
-import static org.bonitasoft.engine.bpm.flownode.HumanTaskInstanceSearchDescriptor.*
 
 public class Index implements RestApiController {
 
@@ -20,8 +11,9 @@ public class Index implements RestApiController {
                              PageContext pageContext,
                              RestApiResponseBuilder apiResponseBuilder,
                              RestApiUtil restApiUtil) {
-		apiResponseBuilder.withResponse(new JsonBuilder([]).toPrettyString());
-		apiResponseBuilder.build();
-        }
+
+        apiResponseBuilder.withResponse(new JsonBuilder([]).toPrettyString());
+        apiResponseBuilder.build();
     }
+
 }
